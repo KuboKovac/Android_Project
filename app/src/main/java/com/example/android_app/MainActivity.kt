29fcfa72d071e60f07dc529f30de6e9f1017c617
KuboKovac <jakub.kovac@kosickaakademia.sk>
2022-12-15 19:26:09 +0100

    I hate android even more :( :(

--- app/src/main/java/com/example/android_app/MainActivity.kt
+++ app/src/main/java/com/example/android_app/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.android_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_app.adapters.VehicleAdapter
+import com.example.android_app.dataClasses.Vehicle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var vehicleList: ArrayList<Vehicle>
+    private lateinit var vehicleAdapter: VehicleAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.carRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        vehicleList = ArrayList()
+
+        //DB code
+
+        vehicleAdapter = VehicleAdapter(vehicleList)
+        recyclerView.adapter = vehicleAdapter
     }
 }
--- app/src/main/java/com/example/android_app/adapters/VehicleAdapter.kt
+++ app/src/main/java/com/example/android_app/adapters/VehicleAdapter.kt
@@ -1,27 +1,31 @@
 package com.example.android_app.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_app.R
 import com.example.android_app.dataClasses.Vehicle
 
 class VehicleAdapter(private val vehicleList:ArrayList<Vehicle>)
     : RecyclerView.Adapter<VehicleAdapter.VehicleViewHolder>() {
 
     class VehicleViewHolder(vehicleView: View) : RecyclerView.ViewHolder(vehicleView){
-
-
+        val nameView : TextView = itemView.findViewById(R.id.nameView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehicleViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.vehicle_item, parent, false)
+        return VehicleViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: VehicleViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val vehicle = vehicleList[position]
+        holder.nameView.text = vehicle.name
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return vehicleList.size
     }
 }
